1415290b16b5d019e94b0b469e9c3c6af0935e82
Juliane Silva <jl.sansil@gmail.com>
2021-05-29 14:03:50 +0100

    feat/section1: update header to be fixed

---
+++ src/components/Layout/Boundary.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Boundary = styled.div`
+  padding-left: 5%;
+  padding-right: 5%;
+
+  @media (min-width: 1066px) {
+    padding-left: calc((100% - 960px) / 2);
+    padding-right: calc((100% - 960px) / 2);
+  }
+
+  @media (min-width: 1440px) {
+    padding-left: calc((100% - 1200px) / 2);
+    padding-right: calc((100% - 1200px) / 2);
+  }
+`;
+
+export default Boundary;
--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import SWORDLogo from '../../assets/images/SWORD_Health_logo.svg';
+import Boundary from './Boundary';
 import Typography from '../Typography';
+import SWORDLogo from '../../assets/images/SWORD_Health_logo.svg';
 
-const FlexContainer = styled.div`
+const FixedHeader = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+`;
+
+const Flex = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -13,21 +21,27 @@ const FlexContainer = styled.div`
 
 const Header: React.FC = () => {
   return (
-    <header>
-      <FlexContainer>
-        <img src={SWORDLogo} alt="SWORD Health logo" height="46px" />
+    <FixedHeader>
+      <Boundary>
+        <Flex>
+          <img src={SWORDLogo} alt="SWORD Health logo" height="46px" />
 
-        <div>
-          <Typography variant="subtitle2" fontStyle="bold">
-            Frontend Developer
-          </Typography>
+          <div>
+            <Typography variant="subtitle2" fontStyle="bold">
+              Frontend Developer
+            </Typography>
 
-          <Typography variant="body" fontStyle="italic" margin={{ top: '4px' }}>
-            Challenge
-          </Typography>
-        </div>
-      </FlexContainer>
-    </header>
+            <Typography
+              variant="body"
+              fontStyle="italic"
+              margin={{ top: '4px' }}
+            >
+              Challenge
+            </Typography>
+          </div>
+        </Flex>
+      </Boundary>
+    </FixedHeader>
   );
 };
 
